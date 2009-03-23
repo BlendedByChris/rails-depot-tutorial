6a3c5a23aa5624a55f3820284877f29159b5a54d
Chris LeBlanc <chris@webPragmatist.com>
2009-03-23 10:07:23 -0500

    Adding empty cart function. Drying up redirect code. Adding total price in views.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -7,9 +7,22 @@ class StoreController < ApplicationController
     product = Product.find(params[:id])
     @cart = find_cart
     @cart.add_product(product)
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index "Invalid product"
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index "Your cart is currently empty"
   end
 
   private
+
+  def redirect_to_index(msg)
+    flash[:notice] = msg
+    redirect_to :action => 'index'
+  end
 
   def find_cart
     session[:cart] ||= Cart.new
