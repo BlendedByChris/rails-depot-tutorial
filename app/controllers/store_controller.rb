8a57b6f5a7d785a795c2276c7cbb2918377860e8
Chris LeBlanc <chris@webPragmatist.com>
2009-03-30 14:36:21 -0500

    Adding ability to convert cart items to db line items and save to a valid order.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -34,6 +34,18 @@ class StoreController < ApplicationController
     end
   end
 
+  def save_order
+    @cart = find_cart
+    @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(@cart)
+    if @order.save
+      session[:cart] = nil
+      redirect_to_index("Thank you for your order")
+    else
+      render :action => 'checkout'
+    end
+  end
+
   private
 
   def redirect_to_index(msg = nil)
@@ -52,5 +64,6 @@ class StoreController < ApplicationController
       session[:counter] += 1
     end
   end
+
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,16 @@ class Order < ActiveRecord::Base
   ]
 
   has_many :line_items
+
+  validates_presence_of :name, :address, :email, :pay_type
+  validates_presence_of :line_items, :message => "can't be empty"
+  validates_inclusion_of :pay_type, :in =>
+    PAYMENT_TYPES.map { |disp, value| value }
+
+  def add_line_items_from_cart(cart)
+    cart.items.each do |item|
+      li = LineItem.from_cart_item(item)
+      line_items << li
+    end
+  end
 end
